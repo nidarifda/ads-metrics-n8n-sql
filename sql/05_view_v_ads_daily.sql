a4def0337a6ea4c80dd318d8209f86ebaf8a58c6
githubunny <131416506+nidarifda@users.noreply.github.com>
2025-09-01 18:51:33 +0800

    Update 05_view_v_ads_daily.sql

--- sql/05_view_v_ads_daily.sql
+++ sql/05_view_v_ads_daily.sql
@@ -1,6 +1,7 @@
+CREATE OR REPLACE VIEW public.v_ads_daily AS
 SELECT
   date, platform, account, campaign, country, device,
   spend, clicks, impressions, conversions,
-  CASE WHEN conversions > 0 THEN spend / conversions END                AS cac,
-  CASE WHEN spend > 0       THEN (conversions * 100.0) / spend END      AS roas
+  CASE WHEN conversions > 0 THEN spend / conversions END                 AS cac,
+  CASE WHEN spend > 0       THEN (conversions * 100.0) / spend END       AS roas
 FROM public.ads_spend;
